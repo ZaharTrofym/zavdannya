da5e6d2e894e82b7dd66238578b7a5c4f0bf287f
Zahar Trofymenko <scoolnick031206@gmail.com>
2022-04-25 15:57:16 +0300

    add strings

---
+++ password.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string latters = "abcdefgABCDEFG";
+    string numbers = "1234567890";
+    string spec = "!@#$%^&*";
+}
